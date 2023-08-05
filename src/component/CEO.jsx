e4924078f1ffe94fb3dd487f417b5e3bd8e49979
kim young ok <kyo2685@didimu.co.kr>
2023-08-05 13:11:21 +0900

    메뉴텝만들기

---
+++ src/component/CEO.jsx
@@ -0,0 +1,5 @@
+export function CEO(){
+    return(
+        <h1>CEO 컴포넌트입니다.</h1>
+    )
+}
--- src/component/Content.js
+++ src/component/Content.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
+import { CEO } from './CEO'
+
 
 export default function Content() {
+  const [currentTab, setCurrentTab] = useState("")
   return (
-    <div>Content입니다</div>
+    <div>
+<section>
+  <button onClick={() => {setCurrentTab("CEO")}}>CEO</button>
+  <button onClick={() => {setCurrentTab("BI")}}>BI</button>
+  <button onClick={() => {setCurrentTab("CAT")}}>CAT</button>
+</section>
+<p>지금 누른 탭 이름은 : {currentTab}</p>
+
+{currentTab === "CEO" && <CEO />}
+{currentTab === "BI" && <h1>BI입니다.</h1>}
+{currentTab === "CAT" && <h1>CAT입니다.</h1>}
+    </div>
   )
 }
