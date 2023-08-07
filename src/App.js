5623105124cbff9a26eb8d5b5395f64b62fcdcab
didimu <49885399+didimu@users.noreply.github.com>
2023-08-07 23:39:33 +0900

    Header, Footer 공통 컴포넌트

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import {BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import { Login } from "./page/Login";
 import { Signup } from "./page/Signup";
+import {Header} from "./component/Header";
+import {Footer} from "./component/Footer";
 
 function App() {
   return (
-    <BrowserRouter>      
+    <BrowserRouter> 
+      <Header />
       <Routes>
         <Route path="/" element = {<Home/>}/>
         <Route path="/login" element = {<Login/>}/>
         <Route path="/signup" element = {<Signup/>}/>
       </Routes>    
+      <Footer/>
     </BrowserRouter>
   );
 }
